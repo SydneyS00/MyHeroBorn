4029392307477e825e89a174f09d09fa515367ee
SydneyS00 <sydschmitz03@outlook.com>
2021-03-16 19:16:08 -0700

    Build Added
    Moved all of build files due to some bug in github.

---
+++ MyHeroBornGame/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Transform patrolRoute;
+    public List<Transform> locations;
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+   void Update()
+    {
+        if(agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+            return;
+
+        agent.destination = locations[locationIndex].position;
+
+        locationIndex = (locationIndex+1) % locations.Count;
+
+    }
+
+
+void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detected - ATTACK!");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol.");
+        }
+    }
+}
